f479ed8a43496142d5bfbaf2fe733837051ce820
Canybardeloton <armel.gilibert@gmail.com>
2024-12-20 11:55:27 +0100

    Toolbox Class

---
+++ Class/toolbox.py
@@ -0,0 +1,22 @@
+class	Toolbox:
+	def	__init__(self):
+		self.Hammers = Hammers
+		self.Screwdrivers = Screwdrivers
+
+class	Hammers :
+	def	__init__(self, color):
+		self.color = color
+	def	painting(self):
+		return
+	def	hammer_in(self):
+		return
+	def	remove_nails(self):
+		return
+
+class	Screwdrivers :
+	def	__init__(self, size):
+		self.size = size * 100
+	def	thighten_screw(self):
+		return
+	def	loosen_screw(self):
+		return
